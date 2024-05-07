af5002f9cf9e63c6ef215707c50b9c98e9b274c2
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-05-07 18:55:31 -0400

    css

--- src/App.js
+++ src/App.js
@@ -4,26 +4,40 @@ import { Options } from "./components/Options";
 
 import "./style.scss";
 
-const FLEX_DIRECTIONS =  ["row", "column"]
+const FLEX_DIRECTIONS = ["row", "column"];
+const CONTENT_JUSTIFICATIONS = ["start", "center", "end", "space-between", "space-around", "space-evenly"];
 
 function App() {
   const [flexDirection, setFlexDirection] = useState(FLEX_DIRECTIONS[0]);
-
-  const setFlexDirectionWrapper = (dir) => {
-    return () => setFlexDirection(dir);
-  };
+  const [justifyContent, setJustifyContent] = useState(CONTENT_JUSTIFICATIONS[0]);
 
   return (
     <div className="App">
-      <Options title="flex-direction" options={FLEX_DIRECTIONS} setOption={setFlexDirection} />
+      <div className="all-options-container">
+        <Options
+          title="flex-direction"
+          options={FLEX_DIRECTIONS}
+          setOption={setFlexDirection}
+          currentOption={flexDirection}
+        />
+        <Options
+          title="justify-content"
+          options={CONTENT_JUSTIFICATIONS}
+          setOption={setJustifyContent}
+          currentOption={justifyContent}
+        />
+      </div>
 
-
-      <div></div>
-
-      <div className={classNames(flexDirection, "blocks")}>
-        <div className="block">  </div>
-        <div className="block">  </div>
-        <div className="block">  </div>
+      <div className={classNames(flexDirection, justifyContent, "blocks")}>
+        <div className="block" key="block1">
+          {" "}
+        </div>
+        <div className="block" key="block2">
+          {" "}
+        </div>
+        <div className="block" key="block3">
+          {" "}
+        </div>
       </div>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
 import classNames from "classnames";
 
-export const Options = ({title, options, setOption}) => {
-
+export const Options = ({ title, options, setOption, currentOption }) => {
   const setOptionWrapper = (option) => {
     return () => setOption(option);
   };
 
-console.log(options)
   return (
     <div className="options-container">
-        <div className="option-title"> {title}</div>
+      <div className="option-title"> {title}</div>
       <div className="options-buttons">
-        {options.map((option) => {
-          return <button onClick={setOptionWrapper((option))} className={classNames("option-button")}>{option}</button>;
+        {options.map((option, idx) => {
+          return (
+            <button
+              key={`${options}${idx}`}
+              onClick={setOptionWrapper(option)}
+              className={classNames("option-button", currentOption === option ? "highlight" : "" )}
+            >
+              {option}
+            </button>
+          );
         })}
       </div>
     </div>
